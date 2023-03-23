188b56df45ba13cb1eaf1b824a4b45a1c35f9490
Steven Guo <steveng20@berkeley.edu>
2023-03-23 15:07:14 -0700

    started

--- ChooseMyRecipe/ContentView.swift
+++ ChooseMyRecipe/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            HomeView().tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+            }
+            
+            SearchView().tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("Search")
+            }
+            
+            FavoritesView().tabItem {
+                Image(systemName: "star")
+                Text("Favorites")
+            }
         }
-        .padding()
     }
 }
 
---
+++ ChooseMyRecipe/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesView.swift
+//  ChooseMyRecipe
+//
+//  Created by Steven Guo on 3/23/23.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritesView()
+    }
+}
