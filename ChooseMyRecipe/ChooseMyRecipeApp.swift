2693ba1db260d5b467693f6746ce66550d643e8d
Steven Guo <steveng20@berkeley.edu>
2023-03-22 23:02:09 -0700

    init

---
+++ ChooseMyRecipe/ChooseMyRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseMyRecipeApp.swift
+//  ChooseMyRecipe
+//
+//  Created by Steven Guo on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ChooseMyRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
